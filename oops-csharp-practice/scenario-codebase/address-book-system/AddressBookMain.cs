5b4bf134c482024812e21bd877dab1c85833dfe7
sakshigautam09 <122722826+sakshigautam09@users.noreply.github.com>
2026-01-14 12:39:43 +0530

    [SakshiGautam]Add.added welcome message

--- oops-csharp-practice/scenario-codebase/address-book-system/AddressBookMain.cs
+++ oops-csharp-practice/scenario-codebase/address-book-system/AddressBookMain.cs
@@ -1,59 +1,9 @@
 using System;
-class AddressBookMain
+public class AddressBookMain
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        // UC-0: Welcome message
-        Console.WriteLine("Welcome to Address Book Program\n");
-        // UC-1: Create a Contact using encapsulated class
-        Console.WriteLine("UC-1: Create Contact\n");
-        Console.Write("Enter First Name: ");
-        string firstName = Console.ReadLine();
-        Console.Write("Enter Last Name: ");
-        string lastName = Console.ReadLine();
-        Console.Write("Enter Address: ");
-        string address = Console.ReadLine();
-        Console.Write("Enter City: ");
-        string city = Console.ReadLine();
-        Console.Write("Enter State: ");
-        string state = Console.ReadLine();
-        Console.Write("Enter Zip: ");
-        string zip = Console.ReadLine();
-        Console.Write("Enter Phone Number: ");
-        string phoneNumber = Console.ReadLine();
-        Console.Write("Enter Email: ");
-        string email = Console.ReadLine();
-
-        // Create Contact object (UC-1)
-        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-        Console.WriteLine("\nContact Created Successfully:");
-
-        Console.WriteLine(contact.GetFirstName() + " " + contact.GetLastName() + ", " + contact.GetCity() + ", " +
-        contact.GetState() + ", " + contact.GetPhoneNumber() + ", " + contact.GetEmail());
-
-
-
-        // Create AddressBook object (UC-2) – has-a relationship
-        AddressBook addressBook = new AddressBook(contact);
-
-        Console.WriteLine("\nContact added successfully!");
-        Console.WriteLine("Contact Details:");
-        Console.WriteLine(contact.GetFirstName() + " " + contact.GetLastName() + ", " + contact.GetCity() + ", " +
-            contact.GetState() + ", " + contact.GetPhoneNumber() + ", " + contact.GetEmail());
-        }
+        // UC-0: Display welcome message
+        Console.WriteLine("Welcome to Address Book Program");
     }
-
-    // AddressBook class (UC-2)
-    public class AddressBook
-    {
-        private Contact contact; // Has-a relationship
-
-        public AddressBook(Contact contact)
-        {
-            this.contact = contact;
-        }
-        public Contact GetContact()
-        {
-            return contact;
-        }
-    }
+}
