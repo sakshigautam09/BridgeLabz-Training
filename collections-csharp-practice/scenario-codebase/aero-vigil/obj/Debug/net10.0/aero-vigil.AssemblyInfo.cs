36ffe704ed5f50da45f6013595b50978718e113d
sakshigautam09 <122722826+sakshigautam09@users.noreply.github.com>
2026-02-05 22:37:45 +0530

    [SakshiGautam]Add.added sonar qube to aero vigil

--- collections-csharp-practice/scenario-codebase/aero-vigil/obj/Debug/net10.0/aero-vigil.AssemblyInfo.cs
+++ collections-csharp-practice/scenario-codebase/aero-vigil/obj/Debug/net10.0/aero-vigil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aero-vigil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bf9708696cbc4838e70cd431fb4784421765e15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f812c22b03ac409966c8cd64795a1e45ceb84094")]
 [assembly: System.Reflection.AssemblyProductAttribute("aero-vigil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aero-vigil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
