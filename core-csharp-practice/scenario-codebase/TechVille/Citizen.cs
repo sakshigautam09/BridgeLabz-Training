abc0c05b9550651ee4fb8a6bef2707f6f0191b8d
sakshigautam09 <122722826+sakshigautam09@users.noreply.github.com>
2026-02-17 11:13:36 +0530

    [SakshiGautam]Add.added module-1 Citizen Registration portal

---
+++ core-csharp-practice/scenario-codebase/TechVille/Citizen.cs
@@ -0,0 +1,10 @@
+namespace TechVille
+{
+    public class Citizen
+    {
+        public string Name;
+        public int Age;
+        public double Income;
+        public int ResidencyYears;
+    }
+}
